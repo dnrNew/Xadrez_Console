066979746f5067b436a88d9b8b76b61c1f423be7
Diego Ribeiro <dnr.new@gmail.com>
2020-02-24 11:45:40 -0300

    Section-1 | Tratando erros com exceções personalizadas
    Tratando erros com exceções personalizadas, foram criados métodos para
    validar a posição das peças do tabuleiro se ela é valida ou não, também
    se posição da peça existe no tabuleiro que tem 8x8, array de 0 a 7.

--- Xadrez_Console/Board/Chessboard.cs
+++ Xadrez_Console/Board/Chessboard.cs
@@ -12,16 +12,45 @@
             this.columns = columns;
             pieces = new Piece[lines, columns];
         }
+        
+        public bool PieceExist(Position position)
+        {
+            ValidationPosition(position);
+            return Piece(position) != null;
+        }
 
         public Piece Piece(int line, int column)
         {
             return pieces[line, column];
         }
 
+        public Piece Piece(Position position)
+        {
+            return pieces[position.line, position.column];
+        }
+
         public void InsertPiece(Piece piece, Position position)
         {
+            if (PieceExist(position))
+                throw new BoardException("Exist piece in position selected!");
+
             pieces[position.line, position.column] = piece;
             piece.position = position;
         }
+
+        public bool PositionValidated(Position position)
+        {
+            if (position.line < 0 || position.line >= lines || position.column < 0 || position.column >= columns)
+                return false;
+
+            return true;
+        }
+
+        public void ValidationPosition(Position position)
+        {
+            if (!PositionValidated(position))
+                throw new BoardException("Position Invalidate!");
+        }
+
     }
 }
--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -8,13 +8,21 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            Chessboard chessboard = new Chessboard(8, 8);
+            try
+            {
+                Chessboard chessboard = new Chessboard(8, 8);
 
-            chessboard.InsertPiece(new Tower(chessboard, Color.Black), new Position(0, 0));
-            chessboard.InsertPiece(new Tower(chessboard, Color.Black), new Position(1, 3));
-            chessboard.InsertPiece(new King(chessboard, Color.Black), new Position(2, 4));
+                chessboard.InsertPiece(new Tower(chessboard, Color.Black), new Position(0, 0));
+                chessboard.InsertPiece(new Tower(chessboard, Color.Black), new Position(1, 3));
+                chessboard.InsertPiece(new King(chessboard, Color.Black), new Position(2, 4));
 
-            Screen.ShowChessboard(chessboard);
+                Screen.ShowChessboard(chessboard);
+                
+            }
+            catch (BoardException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             Console.ReadKey();
         }
